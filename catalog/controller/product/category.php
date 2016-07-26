a87a2065f68613a73b36867f67ba992e30788572
Nill Mefistophell <asklepiy@i.ua>
2016-07-26 13:01:29 +0300

    Added SOLD to category of products

--- catalog/controller/product/category.php
+++ catalog/controller/product/category.php
@@ -212,6 +212,7 @@ class ControllerProductCategory extends Controller {
 
 				$data['products'][] = array(
 					'product_id'  => $result['product_id'],
+					'quantity'  => $result['quantity'],
 					'thumb'       => $image,
 					'name'        => $result['name'],
 					'description' => utf8_substr(strip_tags(html_entity_decode($result['description'], ENT_QUOTES, 'UTF-8')), 0, $this->config->get($this->config->get('config_theme') . '_product_description_length')) . '..',
