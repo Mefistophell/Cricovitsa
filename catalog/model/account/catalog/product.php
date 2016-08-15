a24040e092f05631cdc5cf6df2bf3aec0c79a759
Nill Mefistophell <asklepiy@i.ua>
2016-08-15 23:12:04 +0300

    Fixed to add the seller product

--- catalog/model/account/catalog/product.php
+++ catalog/model/account/catalog/product.php
@@ -1,6 +1,25 @@
 <?php
 class ModelAccountCatalogProduct extends Model {
 	public function addProduct($data) {
+        
+        $data = $data + array(
+            'sku' => '',
+            'upc' => '',
+            'ean' => '',
+            'isbn' => '',
+            'isbn' => '',
+            'mpn' => '',
+            'jan' => '',
+            'location' => '',
+            'manufacturer_id' => '',
+            'tax_class_id' => '',
+            'quantity' => 1,
+            'minimum' => 1,
+            'subtract' => 1,
+            'stock_status_id' => 6,
+            'shipping' => 1,
+        );
+        
     $seller_id = $this->customer->getId(); 
 		$this->db->query("INSERT INTO " . DB_PREFIX . "product SET seller_id = '" . (int)$seller_id . "', model = '" . $this->db->escape($data['model']) . "', sku = '" . $this->db->escape($data['sku']) . "', upc = '" . $this->db->escape($data['upc']) . "', ean = '" . $this->db->escape($data['ean']) . "', jan = '" . $this->db->escape($data['jan']) . "', isbn = '" . $this->db->escape($data['isbn']) . "', mpn = '" . $this->db->escape($data['mpn']) . "', location = '" . $this->db->escape($data['location']) . "', quantity = '" . (int)$data['quantity'] . "', minimum = '" . (int)$data['minimum'] . "', subtract = '" . (int)$data['subtract'] . "', stock_status_id = '" . (int)$data['stock_status_id'] . "', date_available = '" . $this->db->escape($data['date_available']) . "', date_ende = '" . $this->db->escape($data['date_ende']) . "', manufacturer_id = '" . (int)$data['manufacturer_id'] . "', shipping = '" . (int)$data['shipping'] . "', price = '" . (float)$data['price'] . "', points = '" . (int)$data['points'] . "', weight = '" . (float)$data['weight'] . "', weight_class_id = '" . (int)$data['weight_class_id'] . "', length = '" . (float)$data['length'] . "', width = '" . (float)$data['width'] . "', height = '" . (float)$data['height'] . "', length_class_id = '" . (int)$data['length_class_id'] . "', status = '" . (int)$data['status'] . "', tax_class_id = '" . (int)$data['tax_class_id'] . "', sort_order = '" . (int)$data['sort_order'] . "', date_added = NOW()");
 
