4852e795db5fb425d4da3940934f77c8f1369ee5
Nill Mefistophell <asklepiy@i.ua>
2016-08-15 23:11:33 +0300

    Search author

--- catalog/controller/common/search.php
+++ catalog/controller/common/search.php
@@ -7,9 +7,18 @@ class ControllerCommonSearch extends Controller {
 
 		if (isset($this->request->get['search'])) {
 			$data['search'] = $this->request->get['search'];
-		} else {
+             $data['selected_art'] = 'selected="selected"';
+            $data['selected_artist'] = '';
+        } elseif (isset($this->request->get['name'])) {
+            $data['search'] = $this->request->get['name'];
+            $data['selected_artist'] = 'selected="selected"';
+            $data['selected_art'] = '';
+        } else {
 			$data['search'] = '';
+            $data['selected_artist'] = '';
+            $data['selected_art'] = '';
 		}
+        
 
 		return $this->load->view('common/search', $data);
 	}
