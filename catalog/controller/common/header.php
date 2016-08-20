c79b74e59d4e858c43c5bcfe672e8f53a2d5d40f
Nill Mefistophell <asklepiy@i.ua>
2016-08-20 13:10:34 +0300

    Added Category Main

--- catalog/controller/common/header.php
+++ catalog/controller/common/header.php
@@ -164,7 +164,7 @@ class ControllerCommonHeader extends Controller {
 					'name'     => $category['name'],
 					'children' => $children_data,
 					'column'   => $category['column'] ? $category['column'] : 1,
-					'href'     => $this->url->link('product/category', 'path=' . $category['category_id'])
+					'href'     => $this->url->link('product/category', 'main=1&path=' . $category['category_id'])
 				);
 			}
 		}
