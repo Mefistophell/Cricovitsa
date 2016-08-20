973440924e50dd009469a5932c129efdffd80663
Nill Mefistophell <asklepiy@i.ua>
2016-08-20 13:05:54 +0300

    Added Category Main

--- catalog/controller/product/category.php
+++ catalog/controller/product/category.php
@@ -386,8 +386,77 @@ class ControllerProductCategory extends Controller {
 			$data['content_bottom'] = $this->load->controller('common/content_bottom');
 			$data['footer'] = $this->load->controller('common/footer');
 			$data['header'] = $this->load->controller('common/header');
+            
+            if (isset($this->request->get['main'])) {
+                
+                $parent_path = '';
 
-			$this->response->setOutput($this->load->view('product/category', $data));
+                if (isset($this->request->get['path'])) {
+                    $parent_path .= $this->request->get['path'];
+                }
+                
+                if (isset($this->request->get['path'])) {
+                    $parts = explode('_', (string)$this->request->get['path']);
+                } else {
+                    $parts = array();
+                }
+
+                if (isset($parts[0])) {
+                    $data['category_id'] = $parts[0];
+                } else {
+                    $data['category_id'] = 0;
+                }
+
+                if (isset($parts[1])) {
+                    $data['child_id'] = $parts[1];
+                } else {
+                    $data['child_id'] = 0;
+                }
+                $data['categories'] = array();
+
+                $categories = $this->model_catalog_category->getCategoriesWithLevel();
+                
+                foreach ($categories as $category) {
+                    $children_data = array();
+
+                    if ($category['category_id'] == $data['category_id']) {
+                        $children = $this->model_catalog_category->getCategories($category['category_id']);
+
+                        foreach($children as $child) {
+                            $filter_data = array('filter_category_id' => $child['category_id'], 'filter_sub_category' => true);
+
+                            $children_data[] = array(
+                                'category_id' => $child['category_id'],
+                                'name' => $child['name'] . ($this->config->get('config_product_count') ? ' (' . $this->model_catalog_product->getTotalProducts($filter_data) . ')' : ''),
+                                'href' => $this->url->link('product/category', 'path=' . $category['category_id'] . '_' . $child['category_id'])
+                            );
+                        }
+                    }
+
+                    $filter_data = array(
+                        'filter_category_id'  => $category['category_id'],
+                        'filter_sub_category' => true
+                    );
+
+                    $data['categories'][] = array(
+                        'category_id' => $category['category_id'],
+                        'image' => $category['image'],
+                        'popular' => $category['popular'],
+                        'description' => $category['description'],
+                        'level' => $category['level'],
+                        'name'        => $category['name'] . ($this->config->get('config_product_count') ? ' (' . $this->model_catalog_product->getTotalProducts($filter_data) . ')' : ''),
+                        'children'    => $children_data,
+                        'href'        => $this->url->link('product/category', 'path=' . $category['category_id']),
+                        'href_subject'        => $this->url->link('product/category', 'path=' . $parent_path . '_' . $category['category_id']),
+                        'href_medium'        => $this->url->link('product/category', 'path=' . $parent_path . '_0_' . $category['category_id']),
+                        'href_style'        => $this->url->link('product/category', 'path=' . $parent_path . '_0_0_' . $category['category_id']),
+                    );
+                }
+                
+                $this->response->setOutput($this->load->view('product/category_main', $data));
+            } else {
+                $this->response->setOutput($this->load->view('product/category', $data));
+            }
 		} else {
 			$url = '';
 
