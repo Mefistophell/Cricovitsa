9cbcd3db59dc024663ab5ec1abad4e9b3af68921
Nill Mefistophell <asklepiy@i.ua>
2016-08-20 16:06:14 +0300

    Fixed html-content > included main

--- catalog/controller/module/html.php
+++ catalog/controller/module/html.php
@@ -9,7 +9,7 @@ class ControllerModuleHTML extends Controller
             $data['heading_title'] = html_entity_decode($setting['module_description'][$this->config->get('config_language_id')]['title'], ENT_QUOTES, 'UTF-8');
             $data['html'] = html_entity_decode($setting['module_description'][$this->config->get('config_language_id')]['description'], ENT_QUOTES, 'UTF-8');
 
-            if (isset($this->request->get['path'])) {
+            if (isset($this->request->get['path']) && isset($this->request->get['main'])) {
                 $category_id = $this->request->get['path'];
                 $this->load->model('catalog/category');
                 $category = $this->model_catalog_category->getCategory($category_id);
