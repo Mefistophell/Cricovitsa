dcfc3385af789c14e9821a61ac78387da80249cc
Nill Mefistophell <asklepiy@i.ua>
2016-09-01 15:53:01 +0300

    Added news links

--- catalog/controller/common/footer.php
+++ catalog/controller/common/footer.php
@@ -11,7 +11,8 @@ class ControllerCommonFooter extends Controller {
 		$data['text_contact'] = $this->language->get('text_contact');
 		$data['text_return'] = $this->language->get('text_return');
 		$data['text_sitemap'] = $this->language->get('text_sitemap');
-		$data['text_manufacturer'] = $this->language->get('text_manufacturer');
+		$data['text_artist'] = $this->language->get('text_artist');
+		$data['text_news'] = $this->language->get('text_news');
 		$data['text_voucher'] = $this->language->get('text_voucher');
 		$data['text_affiliate'] = $this->language->get('text_affiliate');
 		$data['text_special'] = $this->language->get('text_special');
@@ -36,7 +37,8 @@ class ControllerCommonFooter extends Controller {
 		$data['contact'] = $this->url->link('information/contact');
 		$data['return'] = $this->url->link('account/return/add', '', true);
 		$data['sitemap'] = $this->url->link('information/sitemap');
-		$data['manufacturer'] = $this->url->link('product/manufacturer');
+		$data['artist'] = $this->url->link('product/author');
+		$data['artist'] = $this->url->link('information/news');
 		$data['voucher'] = $this->url->link('account/voucher', '', true);
 		$data['affiliate'] = $this->url->link('affiliate/account', '', true);
 		$data['special'] = $this->url->link('product/special');
@@ -47,6 +49,63 @@ class ControllerCommonFooter extends Controller {
 
 		$data['powered'] = sprintf($this->language->get('text_powered'), $this->config->get('config_name'), date('Y', time()));
 
+        // Menu
+		$this->load->model('catalog/category');
+
+		$this->load->model('catalog/product');
+
+		$data['categories'] = array();
+
+		$categories = $this->model_catalog_category->getCategories(0);
+
+		foreach ($categories as $category) {
+			if ($category['top']) {
+				// Level 2
+				$children_data = array();
+
+				//$children = $this->model_catalog_category->getCategories($category['category_id']);
+                $children = $this->model_catalog_category->getMultiParentCategories($category['category_id']);
+
+				foreach ($children as $child) {
+					$filter_data = array(
+						'filter_category_id'  => $child['category_id'],
+						'filter_sub_category' => true
+					);
+                    
+                    $paths = '';
+                    
+                    switch ($child['level']) {
+                    case 0:
+                        $paths = $category['category_id'] . '_' . $child['category_id'];
+                        break;
+                    case 1:
+                       $paths = $category['category_id'] . '_' . $child['category_id'];
+                        break;
+                    case 2:
+                        $paths = $category['category_id'] . '_0_' . $child['category_id'];
+                        break;
+                    case 3:
+                        $paths = $category['category_id'] . '_0_0_' . $child['category_id'];
+                        break;
+}
+
+					$children_data[] = array(
+						'name'  => $child['name'] . ($this->config->get('config_product_count') ? ' (' . $this->model_catalog_product->getTotalProducts($filter_data) . ')' : ''),
+						'href'  => $this->url->link('product/category', 'path=' . $paths),
+                        'level' => $child['level']
+					);
+				}
+
+				// Level 1
+				$data['categories'][] = array(
+					'name'     => $category['name'],
+					'children' => $children_data,
+					'column'   => $category['column'] ? $category['column'] : 1,
+					'href'     => $this->url->link('product/category', 'main=1&path=' . $category['category_id'])
+				);
+			}
+		}
+        
 		// Whos Online
 		if ($this->config->get('config_customer_online')) {
 			$this->load->model('tool/online');
